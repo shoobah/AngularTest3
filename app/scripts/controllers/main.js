019eb4f641de1ecb349c9655dea649ece054baf0
shoobah <shoobah@gmail.com>
2013-11-12 00:09:28 +0100

    First

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularTest3App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
